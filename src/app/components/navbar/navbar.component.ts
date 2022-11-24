a827e356c6fbe3bfe4780c72646afbb596140c79
onemmanuel4christ <onemmanuel4christ@gmail.com>
2022-11-24 23:17:50 +0100

    sticky navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { window } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
@@ -8,16 +9,18 @@ import { Component, OnInit } from '@angular/core';
 export class NavbarComponent implements OnInit {
 
   isMenuOpen = false;
+  innerWidth: any;
 
   constructor() { }
 
   ngOnInit(): void {
-
   }
 
   handleOpenMenu(){
     this.isMenuOpen = !this.isMenuOpen
-    console.log(this.isMenuOpen)
+    if(this.isMenuOpen){
+
+    }
   }
 
 }
